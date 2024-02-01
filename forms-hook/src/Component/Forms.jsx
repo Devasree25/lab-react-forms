824159acdd312332544c613bcabc82fbf4b992fe
kalpana-85 <devasree.a@kalvium.community>
2024-02-01 17:37:33 +0530

    form-library

--- forms-hook/src/Component/Forms.jsx
+++ forms-hook/src/Component/Forms.jsx
@@ -1,121 +1,80 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
-const Forms =()=>{
+const Form = () => {
 
-    const [formData, setFormData] = useState({
-        firstname : "",
-        lastname : "",
-        email : "",
-        phoneNumber : ""
-    })
+    const { register, handleSubmit, formState: { errors, isSubmitSuccessful, isSubmitted } } = useForm();
 
-    const [formError, setFormError] = useState({})
-    const [fSubmit, setFSubmit] = useState(false)
 
-    const handleInput =(e)=>{
-       let {name, value} =  e.target
-       setFormData({
-        ...formData, 
-        [name] : value
-       })
+    const handleSubmitForm = (data) => {
+        console.log(data)
     }
 
-    const formSubmit =(e)=>{
-        e.preventDefault();
-        let errors = validate(formData)
-        console.log(errors)
-        setFormError(errors);
 
-        let errorKey = Object.keys(errors);
-        if(errorKey.length == 0){
-            // let the user to register
-            setFSubmit(true)
-        }else{
-            setFSubmit(false)
-        }
+    console.log(isSubmitSuccessful)
 
-
-    }
-
-    const validate =(data)=>{
-        let error = {}
-        if(data.firstname.trim() == ""){
-           error.firstname = "Please enter the firstname"
-        }
-        if(data.lastname.trim() == ""){
-            error.lastname = "Please enter the lastname"
-        }
-        if(data.email.trim() == ""){
-            error.email = "Please enter the email"
-        }
-        if(data.phoneNumber.trim() == "" || data.phoneNumber.trim().length < 10){
-            error.phoneNumber = "Please enter the phone number"
-        }
-        return error;
-    }
-   
-
-   
     return (
         <>
-        <div className="form-parent">
-            <form onSubmit={formSubmit}>
-                <div>
-                    {fSubmit && <p id="p">Sucessfully Registered 😊!</p> }
-              
-                    
-                </div>
-                <h2 id="h2">REGISTRATION FORM</h2>
+            <div className="parent-container">
+                <h1>Registration Form</h1>
+                <form  id="main-form" onSubmit={handleSubmit(handleSubmitForm)}>
+                    { isSubmitSuccessful && <p>Registration is Successful 🤩!</p>}
+                    <label htmlFor="firstname">Enter firstname : </label>
+                    <input type="text" placeholder="firstname"
+                        {...register("firstname", {
+                            required: "firstname is required"
+                        })}
+                    />
+                    {errors.firstname && <p className="error-message">{errors.firstname.message}</p>}
 
-                <div className="box">
-                <label htmlFor="firstname">Enter first name</label>
-                <input type="text"id="text" name="firstname" placeholder="first name"
-                onChange={handleInput}
-                style={{ textAlign: "center" }}
-                />
-                {formError.firstname ? <p>{formError.firstname}</p> : ""}
-                </div>
-
-                
-
-               <div className="box2">
-               <label htmlFor="lastname">Enter last name</label>
-                <input type="text" id="text" name="lastname" placeholder="last name"
-                onChange={handleInput}
-                style={{ textAlign: "center" }}
-                />
-                {formError.lastname ? <p>{formError.lastname}</p> : ""}
-               </div>
-
-               <div className="box3">
-               <label htmlFor="email">Enter email</label>
-                <input type="email" id="text"  name="email" placeholder="email"
-                onChange={handleInput}
-                style={{ textAlign: "center" }}
-                />
-                {formError.email ? <p>{formError.email}</p> : ""}
-
-               </div>
-
-               <div className="box4">
-               <label htmlFor="phoneNumber">Enter mobile number</label>
-                <input type="number" id="text"  name="phoneNumber" placeholder="phone number"
-                onChange={handleInput}
-                style={{ textAlign: "center" }}
-                />
-                {formError.phoneNumber ? <p>{formError.phoneNumber}</p> : ""}
+                    <label htmlFor="lastname">Enter lastname : </label>
+                    <input type="text" placeholder="lastname"
+                        {...register("lastname", {
+                            required: "lastname is required"
+                        })}
+                    />
+                    {errors.lastname && <p className="error-message">{errors.lastname.message}</p>}
 
 
-               </div>
 
-                
+                    <label htmlFor="email">Enter email : </label>
+                    <input type="email" placeholder="enter email"
+                        {...register("email", {
+                            required: "Email is required",
+                            pattern: {
+                                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,}/,
+                                message: "Format is not correct"
+                            }
+                        })}
 
-                <input type="submit" id="text1" value={"Register"} />
-            </form>
-        </div>
+                    />
+                    {errors.email && <p className="error-message">{errors.email.message}</p>}
+
+
+                    <label htmlFor="password">Enter password : </label>
+                    <input type="password" placeholder="enter password"
+                        {...register("password", {
+                            required: "password is required",
+                            minLength: {
+                                value: 5,
+                                message: "minimum length should be 5"
+                            },
+                            pattern: {
+                                value : /[a-zA-Z0-9]/,
+                                message: "Password should be minimum 5 to 20 words"
+                            }
+                        })}
+
+                    />
+                    {errors.password && <p className="error-message">{errors.password.message}</p>}
+
+
+                    <input type="submit" value={"Register"} />
+                </form>
+            </div>
 
         </>
     )
 }
 
-export default Forms;
+export default Form;
